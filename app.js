2a4e1aedcc6b5c545f76b3c4ed096cb0a7ae030e
trinitysilvia <trinitysilviag@gmail.com>
2019-07-25 10:32:14 -0500

    se agregan comentarios de la funcion fecha

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //Mostrar fecha
+//Creando array para los meses y dias
 const month = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio",
 "Julio","Agosto", "Septiembre","Octubre","Noviembre","Diciembre");
 
@@ -6,6 +7,7 @@ const days= new Array ("Domingo", "Lunes", "Martes","Miércoles", "Jueves",
 "Viernes", "Sábado", "Domingo");
 
 const f= new Date(); //"Date" es un constructor
+//imprimiendo fecha 
 const viewData = `<div>
                         ${days[f.getDay()]}
                         ${f.getDate()}
