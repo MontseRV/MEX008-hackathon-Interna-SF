cefaf0b3229c9a87fc2498a9980b38d8e92a0b3d
trinitysilvia <trinitysilviag@gmail.com>
2019-07-25 00:36:11 -0500

    se agrega funcion para visualizar fecha

---
+++ app.js
@@ -0,0 +1,17 @@
+//Mostrar fecha
+const month = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio",
+"Julio","Agosto", "Septiembre","Octubre","Noviembre","Diciembre");
+
+const days= new Array ("Domingo", "Lunes", "Martes","Miércoles", "Jueves",
+"Viernes", "Sábado", "Domingo");
+
+const f= new Date(); //"Date" es un constructor
+const viewData = `<div>
+                        ${days[f.getDay()]}
+                        ${f.getDate()}
+                        ${month[f.getMonth()]}
+                        ${f.getFullYear()} </div>`;
+
+const date= document.getElementById('date').innerHTML=viewData;
+
+
